8ef64d1dfa82d89ed04804a00ea3738d3ce91e42
Vasiliy Girichev <girichev2013@yandex.ru>
2022-12-04 16:20:55 +0500

    MusicPlayer Autowired and Qualifier. Enum. Value

--- src/main/java/com/girichevvasiliy/musicplayer/MusicPlayer.java
+++ src/main/java/com/girichevvasiliy/musicplayer/MusicPlayer.java
@@ -3,6 +3,7 @@ package com.girichevvasiliy.musicplayer;
 import lombok.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -16,16 +17,16 @@ public class MusicPlayer {
     private List<Music> musicList = new ArrayList<>();
     private Music music1;
     private Music music2;
+    @Value("${musicPlayer.name}")
     private String name;
+    @Value("${musicPlayer.volume}")
     private int volume;
 
     @Autowired
     public MusicPlayer(@Qualifier("classicalMusic") Music music1, @Qualifier("rockMusic") Music music2) {
         this.music1 = music1;
         this.music2 = music2;
-        this.name = "AIMP";
-        this.volume = 35;
-    }
+         }
 
     public List<Music> getMusicList() {
         return musicList;
